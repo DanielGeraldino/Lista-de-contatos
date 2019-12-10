be424c6617eb63efd569da63e12dfa240f0a60dd
Daniel Geraldino Guerra <danielgguerra2@gmail.com>
2019-12-10 19:25:04 -0300

    Criando o metodo para buscar image na galeria

--- app/src/main/java/com/example/listacontato/activity/CadastroActivity.kt
+++ app/src/main/java/com/example/listacontato/activity/CadastroActivity.kt
@@ -1,6 +1,9 @@
 package com.example.listacontato.activity
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -11,6 +14,7 @@ import com.example.listacontato.Helper.DataBaseContato
 import com.example.listacontato.R
 import com.example.listacontato.model.Contato
 import kotlinx.android.synthetic.main.activity_cadastro.*
+import java.util.jar.Manifest
 import kotlin.properties.Delegates
 
 class CadastroActivity : AppCompatActivity() {
@@ -37,6 +41,63 @@ class CadastroActivity : AppCompatActivity() {
         nome.setText(textoNome)
         numero.setText(textoNumero)
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //PERMISSÃO CONCEDIDA
+                    selecionaImagemGaleria()
+                } else {
+                    Toast.makeText(applicationContext, "Permissão negada para seleciona imagem!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imageView.setImageURI(data?.data)
+        }
+    }
+
+    fun adicionarFoto(v: View){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // Verifica se tem permissão
+            if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED) {
+                //Solicita permissão
+                val permission = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                requestPermissions(permission, PERMISSION_CODE)
+            } else {
+                // TEM PERMISSÃO
+                selecionaImagemGaleria()
+            }
+        } else {
+            selecionaImagemGaleria()
+        }
+    }
+
+    fun selecionaImagemGaleria(){
+
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    companion object {
+        private val IMAGE_PICK_CODE = 1000
+        private val PERMISSION_CODE = 1001
     }
 
     fun deletaContato(v: View){
